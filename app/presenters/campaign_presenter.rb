5c369c10b0ccc6955f9b379d82a869f0e53e3751
Ritchie Paul Buitre <ritchie@richorelse.com>
2021-02-23 03:12:28 +0800

    Refactor CampaignPresenter

--- app/presenters/campaign_presenter.rb
+++ app/presenters/campaign_presenter.rb
@@ -1,13 +1,13 @@
 class CampaignPresenter < ApplicationPresenter
   def import_candidates_failure_message
-    JSON(import_candidates_failure).dig('error', 'message') if import_candidates_failure?
-  rescue JSON::JSONError
+    JSON(import_candidates_failure).dig('error', 'message')
+  rescue
     import_candidates_failure
   end
 
   def last_import_in_words
-  	return 'never' unless import_candidates_at?
+    return 'never' unless import_candidates_at?
 
-  	"#{h.time_ago_in_words(import_candidates_at)} ago"
+    "#{h.time_ago_in_words(import_candidates_at)} ago"
   end
 end
